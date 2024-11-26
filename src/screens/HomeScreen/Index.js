8493d22b31ece91f9d69f755c46e99d5ad926520
Vítor Azevedo <vigsoft410@gmail.com>
2024-11-26 20:43:27 -0300

    :sparkles: Finishing new image generator using pollinations

--- src/screens/HomeScreen/Index.js
+++ src/screens/HomeScreen/Index.js
@@ -1,49 +1,137 @@
-// src/components/PromptForm.js
-import React, { useState } from "react";
-import Card from "../../components/Card/Index";
-import { CardContainer, MainContainer, Subtitle, Title } from "./styles";
+import React, { useState, useEffect } from "react";
 import { usePollinationsImage } from "@pollinations/react";
+import {
+  CardContainer,
+  GenerateButton,
+  GeneratedImage,
+  GeneratedImagesContainer,
+  MainContainer,
+  Subtitle,
+  Title,
+} from "./styles";
+import Card from "../../components/Card/Index";
+import EmptyImagePreset from "../../components/EmptyImagePreset/Index";
 
-function PromptForm({ setImage, handleSubmit }) {
+function PromptForm() {
   /** States */
   const [prompt, setPrompt] = useState("");
-  const [generatedImage, setGeneratedImage] = useState("");
+  const [truePrompt, setTruePrompt] = useState("");
+  const [images, setImages] = useState([
+    { index: 0, image: "", isLoaded: false },
+    { index: 1, image: "", isLoaded: false },
+    { index: 2, image: "", isLoaded: false },
+  ]);
+  const [generateImages, setGenerateImages] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
-  const image = usePollinationsImage(prompt, {
-    width: 800,
-    height: 600,
+  /** Hooks for generating images */
+  const image1 = usePollinationsImage(generateImages ? truePrompt : null, {
+    width: 600,
+    height: 800,
     seed: 42,
   });
+  const image2 = usePollinationsImage(generateImages ? truePrompt : null, {
+    width: 600,
+    height: 800,
+    seed: 43,
+  });
+  const image3 = usePollinationsImage(generateImages ? truePrompt : null, {
+    width: 600,
+    height: 800,
+    seed: 44,
+  });
 
-  /** Functions */
+  /** Effect to update images when new ones are generated */
+  useEffect(() => {
+    if (generateImages) {
+      setImages((prev) => [
+        { index: 0, image: image1 || "", isLoaded: prev[0].isLoaded },
+        { index: 1, image: image2 || "", isLoaded: prev[1].isLoaded },
+        { index: 2, image: image3 || "", isLoaded: prev[2].isLoaded },
+      ]);
+    }
+  }, [image1, image2, image3, generateImages]);
+
+  /** Handlers */
   const handleGenerate = () => {
-    if (image) {
-      setGeneratedImage(image);
+    // All this condition because pollinations only use hooks to make the requisiton :(
+    if (prompt.length > 3) {
+      setTruePrompt(prompt)
+      setIsLoading(true)
+      setGenerateImages(true); // Trigger image generation
     }
   };
 
   /** Renders */
   const renderImagePrompt = () => (
-    <div>
-      <input onChange={(event) => setPrompt(event.target.value)} />
-      <button onClick={handleGenerate}>Generate</button>
+    <div className="form__group field">
+      <input
+        placeholder="Describe your image..."
+        name="prompt"
+        id="prompt"
+        required
+        className="form__field"
+        onChange={(event) => setPrompt(event.target.value)}
+        style={{ color: "#1a1a1a" }}
+        value={prompt}
+        disabled={isLoading}
+      />
+      <label htmlFor="prompt" className="form__label">
+        Prompt
+      </label>
+      <GenerateButton onClick={handleGenerate}>{isLoading ? 'Generating...' : 'Generate'}</GenerateButton>
     </div>
   );
 
-  const renderImage = () =>
-    generatedImage && <img src={generatedImage} alt="AI Generated" />;
+  const renderEmptyImage = (key) => <EmptyImagePreset key={key} />;
+
+  const handleOnLoad = (src) => {
+    const imgIndex = images.findIndex((img) => img.image === src);
+    if (imgIndex !== -1) {
+      setImages((prevImages) => {
+        const newImages = [...prevImages];
+        newImages[imgIndex] = { ...newImages[imgIndex], isLoaded: true };
+
+        if (newImages.every((val) => val.isLoaded)) {
+          setIsLoading(false)
+        }
+
+        return newImages;
+      });
+    }
+  };
+
+  const renderGeneratedImages = () => {
+    return images.map((value) => {
+      return (
+        <React.Fragment key={value.index}>
+          {value.isLoaded === false ? renderEmptyImage(value.index) : null}
+          <GeneratedImage
+            onLoad={(event) => handleOnLoad(event.target.src)}
+            onError={(err) => console.error("Unexpected error at trying to load image: ", err)}
+            alt=""
+            src={value.image}
+            isHidden={!value.isLoaded}
+          />
+        </React.Fragment>
+      );
+    });
+  };
 
   return (
     <MainContainer>
       <Card>
         <CardContainer>
-          <Title>Image generator</Title>
-          <Subtitle>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-          </Subtitle>
-
+          <div style={{ display: "flex", flexDirection: "column" }}>
+            <Title className="lato-black">Image generator</Title>
+            <Subtitle>
+              Unleash Creativity: Transform Words into Stunning Visuals with AI
+            </Subtitle>
+          </div>
           {renderImagePrompt()}
-          {renderImage()}
+          <GeneratedImagesContainer>
+            {renderGeneratedImages()}
+          </GeneratedImagesContainer>
         </CardContainer>
       </Card>
     </MainContainer>
--- src/screens/HomeScreen/styles.js
+++ src/screens/HomeScreen/styles.js
@@ -4,23 +4,79 @@ export const MainContainer = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  
+
   position: fixed;
   top: 0;
   left: 0;
   width: 100vw;
   height: 100vh;
 
-  background-color: hsl(0, 0%, 10%);
+  background: rgb(26, 26, 26);
+  background: linear-gradient(
+    336deg,
+    rgba(26, 26, 26, 1) 0%,
+    rgba(106, 30, 85, 1) 100%,
+    rgba(106, 30, 85, 1) 100%
+  );
 `;
 
 export const CardContainer = styled.div`
   display: flex;
   flex-direction: column;
+  align-items: center;
+  justify-content: space-evenly;
 
-  min-width: 50vw;
+  min-width: 45vw;
+  min-height: 70vh;
+  gap: 12px;
 `;
 
-export const Title = styled.span``;
+export const GeneratedImage = styled.img`
+  visibility: visible;
+  height: 280px;
+  width: 200px;
+
+  ${(props) =>
+    props.isHidden &&
+    `
+    visibility: hidden;
+    position: absolute;
+    height: 0px;
+    width: 0px;
+  `};
+`;
+export const Title = styled.span`
+  font-size: 32px;
+  color: #6a1e55;
+`;
 
 export const Subtitle = styled.span``;
+
+export const GenerateButton = styled.span`
+  cursor: pointer;
+
+  display: inline-block;
+  border-radius: 28px;
+  font-family: Arial;
+  font-size: 19px;
+
+  width: 120px;
+
+  text-decoration: none;
+  background-color: #6a1e55;
+  color: #fefefe;
+
+  padding: 16px 31px;
+  margin-top: 16px;
+
+  &:hover {
+    background-color: #802b68;
+    transition: 0.3s;
+  }
+`;
+
+export const GeneratedImagesContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 16px;
+`;
