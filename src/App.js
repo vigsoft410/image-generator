024ab471a26ae03b412361c000dcd4decd90fb07
Vítor Azevedo <vigsoft410@gmail.com>
2024-11-26 21:17:23 -0300

    :emergency: Fixing build error

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // src/App.js
-import React, { useState } from 'react';
+import React from 'react';
 import Home from './screens/HomeScreen/Index';
 import './App.css';
 
--- src/components/EmptyImagePreset/Index.js
+++ src/components/EmptyImagePreset/Index.js
@@ -3,7 +3,7 @@ import React from "react";
 import { MainContainer } from "./Styles";
 import { RiImage2Fill } from "@remixicon/react";
 
-const EmptyImagePreset = ({ children }) => {
+const EmptyImagePreset = () => {
   return (
     <MainContainer>
       <RiImage2Fill size={36} color="white" className="ri-image-fill" />
